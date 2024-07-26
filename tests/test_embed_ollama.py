e09c3e42f035ce8cdc56be7a12a48df4658bedd9
Sukhbinder Singh <sukh2010@yahoo.com>
2024-07-26 14:40:57 +0530

    Update test_embed_ollama.py

--- tests/test_embed_ollama.py
+++ tests/test_embed_ollama.py
@@ -5,27 +5,27 @@ from llm.plugins import pm
 import pytest
 import os
 
-
+IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 
 def test_plugin_is_installed():
     names = [mod.__name__ for mod in pm.get_plugins()]
     assert "llm_embed_ollama" in names
     
-@pytest.mark.skipif(not "GITHUB_RUN_ID" in os.environ, reason="Not running on GitHub")
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Not running on GitHub")
 def test_ollama_embed_small():
     model = llm.get_embedding_model("all-minilm")
     floats = model.embed("hello world")
     assert len(floats) == 384
     assert all(isinstance(f, float) for f in floats)
 
-@pytest.mark.skipif(not "GITHUB_RUN_ID" in os.environ, reason="Not running on GitHub")
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Not running on GitHub")
 def test_ollama_embed_long_string():
     model = llm.get_embedding_model("all-minilm")
     outputs = list(model.embed_multi(["a" * 9000, "two"]))
     for floats in outputs:
         assert len(floats) == 384
 
-@pytest.mark.skipif(not "GITHUB_RUN_ID" in os.environ, reason="Not running on GitHub")
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Not running on GitHub")
 def test_ollama_embed_multi(tmpdir):
     db_path = str(tmpdir / "test.db")
     runner = CliRunner()
